dff0e2b42321f6dde18dc995848057fce9ae20b6
Anne <amusau8@gmail.com>
2021-12-09 01:56:41 +0300

    Fixing Error

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Response } from "@angular/http";
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Album } from "./album";
 @Injectable()
@@ -9,8 +10,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id:number) {
-      return this._http.get(this._albumUrl).map(response=> response.json())
+  getAlbum(id:number): Observable<Album> {
+      return this._http.get(this._albumUrl).map(response=> <Album>response.json())
   }
 
 }
